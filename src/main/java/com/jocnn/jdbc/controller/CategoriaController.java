ceaf591b769c3bb2f66b39f36cb595fa2969941c
jocnn <ijann_@outlook.com>
2023-01-02 17:34:44 -0300

    jframe - formulario de producto

---
+++ src/main/java/com/jocnn/jdbc/controller/CategoriaController.java
@@ -0,0 +1,16 @@
+package com.jocnn.jdbc.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaController {
+	public List<?> listar() {
+		// TODO
+		return new ArrayList<>();
+	}
+
+    public List<?> cargaReporte() {
+        // TODO
+        return new ArrayList<>();
+    }
+}
