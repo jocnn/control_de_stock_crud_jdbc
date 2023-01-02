8057c9508e8fd8015017a5e7a1f28d94acbfe967
jocnn <ijann_@outlook.com>
2023-01-02 20:15:31 -0300

    método listar - se retorno un listado de mapa

--- src/main/java/com/jocnn/jdbc/controller/ProductoController.java
+++ src/main/java/com/jocnn/jdbc/controller/ProductoController.java
@@ -1,7 +1,14 @@
 package com.jocnn.jdbc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductoController {
 	public void modificar(String nombre, String descripcion, Integer id) {
@@ -12,9 +19,32 @@ public class ProductoController {
 		// TODO
 	}
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+	public List<Map<String, String>> listar() throws SQLException {
+		Connection cn = DriverManager.getConnection(
+			"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+			"root",
+			"ijann90210");
+		
+		Statement statement = cn.createStatement();
+		boolean result = statement.execute("SELECT id, nombre, descripcion, cantidad FROM producto");
+		
+		ResultSet resultSet = statement.getResultSet();
+		
+		List<Map<String, String>> resultado = new ArrayList<>();
+		
+		while (resultSet.next()) {
+			Map<String, String> fila = new HashMap<>();
+			fila.put("id", String.valueOf(resultSet.getInt("id")));
+			fila.put("nombre", resultSet.getString("nombre"));
+			fila.put("descripcion", resultSet.getString("descripcion"));
+			fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
+
+			resultado.add(fila);
+		}
+		
+		cn.close();
+		
+		return resultado;
 	}
 
     public void guardar(Object producto) {
