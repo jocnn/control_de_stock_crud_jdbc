0dd3a0e5393f7fda4f7d6e3d7a950c225802d86c
jocnn <ijann_@outlook.com>
2023-01-09 14:32:38 -0300

    se agregó y listó productos desde el modelo

--- src/main/java/com/jocnn/jdbc/modelo/Categoria.java
+++ src/main/java/com/jocnn/jdbc/modelo/Categoria.java
@@ -1,8 +1,12 @@
 package com.jocnn.jdbc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 	private Integer id;
 	private String nombre;
+	private List<Producto> productos;
 	
 	public Categoria(Integer id, String nombre) {
 		this.id = id;
@@ -28,6 +32,18 @@ public class Categoria {
 	@Override
 	public String toString() {
 		return this.getNombre();
+	}
+	
+	public void agregar(Producto producto) {
+		if (this.productos == null) {
+			this.productos = new ArrayList<>();
+		}
+		
+		this.productos.add(producto);
+	}
+
+	public List<Producto> getProductos() {
+		return this.productos;
 	}
 	
 }
