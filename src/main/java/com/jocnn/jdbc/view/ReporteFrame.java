31d8b5454dfd32939e09ebaa176e9d8ec076c8cd
jocnn <ijann_@outlook.com>
2023-01-09 15:06:23 -0300

    se cargo reporte de categoria con sus productos

--- src/main/java/com/jocnn/jdbc/view/ReporteFrame.java
+++ src/main/java/com/jocnn/jdbc/view/ReporteFrame.java
@@ -20,7 +20,7 @@ public class ReporteFrame extends JFrame  {
         super("Reporte de produtos del stock");
 		
         this.categoriaController = new CategoriaController();
-		
+  
         Container container = getContentPane();
         setLayout(null);
 		
@@ -44,8 +44,18 @@ public class ReporteFrame extends JFrame  {
     private void cargaReporte() {
         var contenido = categoriaController.cargaReporte();
         
-        // TODO
-        contenido.forEach(fila -> modelo
-                .addRow(new Object[] {}));
+        contenido.forEach(categoria -> {
+            modelo.addRow(new Object[] { categoria });
+            var productos = categoria.getProductos();
+
+            productos.forEach(producto -> modelo.addRow(
+                new Object[] {
+                    "",
+                    producto.getNombre(),
+                    producto.getCantidad()
+                }
+            ));
+
+        });
     }
 }
